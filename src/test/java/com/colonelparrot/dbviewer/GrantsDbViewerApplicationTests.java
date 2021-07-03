97661b641c6fe07f84488851a455ba80bf1fb826
ColonelParrot <65585002+ColonelParrot@users.noreply.github.com>
2021-07-03 19:36:55 -0400

    Application, ApplicationTests

---
+++ src/test/java/com/colonelparrot/dbviewer/GrantsDbViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.colonelparrot.dbviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrantsDbViewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
