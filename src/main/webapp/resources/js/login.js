1550edb1b94b5a898c9a673346f25252914165ef
LieutenantPeacock <lieutenant.peacock@gmail.com>
2022-07-31 17:29:31 -0400

    login.js: remove ?error from URL and display error message for invalid credentials.

--- src/main/webapp/resources/js/login.js
+++ src/main/webapp/resources/js/login.js
@@ -4,6 +4,10 @@
 const loginForm = document.querySelector('form#loginForm');
 const errorMessage = document.getElementById("login-error");
 const loadingLayer = document.querySelector('.loading-layer');
+if (location.search === '?error') {
+	history.replaceState(null, '', location.href.split('?')[0]);
+	errorMessage.textContent = 'Invalid credentials.';
+}
 loginForm.addEventListener('submit', function(e) {
 	errorMessage.textContent = '';
 	if (!this.username.value) {
