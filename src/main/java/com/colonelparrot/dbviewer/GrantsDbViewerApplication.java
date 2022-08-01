ee8234031a7bfa1ca7bb526364144319ab9f6155
LieutenantPeacock <lieutenant.peacock@gmail.com>
2022-08-01 16:00:57 -0400

    Move temporary password generation from WebSecurityConfig to main Spring Boot application class.

--- src/main/java/com/colonelparrot/dbviewer/GrantsDbViewerApplication.java
+++ src/main/java/com/colonelparrot/dbviewer/GrantsDbViewerApplication.java
@@ -1,25 +1,74 @@
 package com.colonelparrot.dbviewer;
 
+import java.io.File;
+import java.util.concurrent.ThreadLocalRandom;
+
+import javax.annotation.PostConstruct;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import com.ltpeacock.dbviewer.commons.ANSIEscapes;
 import com.ltpeacock.dbviewer.commons.AppProperties;
+import com.ltpeacock.dbviewer.commons.SetupInfo;
+import com.ltpeacock.dbviewer.db.repository.AppUserRepository;
 
 /**
  * @author ColonelParrot
  * @version 1.1
  */
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class, 
-	scanBasePackages = {"com.ltpeacock.dbviewer", "com.colonelparrot.dbviewer"})
+@SpringBootApplication(scanBasePackages = {"com.ltpeacock.dbviewer", "com.colonelparrot.dbviewer"})
 @EnableConfigurationProperties(AppProperties.class)
 public class GrantsDbViewerApplication {
 	private static final Logger LOG = LoggerFactory.getLogger(GrantsDbViewerApplication.class);
+	@Autowired
+	private AppUserRepository userRepository;
+	@Autowired
+	private SetupInfo setupInfo;
+	@Autowired
+	private AppProperties appProps;
 
 	public static void main(String[] args) {
 		SpringApplication.run(GrantsDbViewerApplication.class, args);
+	}
+
+	@PostConstruct
+	public void setup() {
+		final File dbViewerBase = new File(appProps.getAppRoot());
+		LOG.info("DB Viewer base folder located at [{}]", dbViewerBase.getAbsolutePath());
+		if (!dbViewerBase.exists()) {
+			LOG.info(ANSIEscapes.YELLOW + "[STARTUP CONFIGURATION]" + ANSIEscapes.RESET
+					+ " No existing base folder found; automatically creating one.");
+			if (!dbViewerBase.mkdirs())
+				LOG.error("Could not create DB Viewer base folder!");
+		}
+		if (userRepository.count() == 0) {
+			LOG.info("New installation detected: there are currently no users.");
+			final String password = generateRandomString(8, false);
+			LOG.info("Generated temporary admin password " + ANSIEscapes.YELLOW + "[{}]" + ANSIEscapes.RESET, password);
+			setupInfo.setAdminPassword(password);
+		}
+	}
+
+	private static String generateRandomString(final int length, final boolean latinCharacters) {
+		int lowerBound = 34;
+		int upperBound = 125;
+		if (latinCharacters) {
+			lowerBound = 97;
+			upperBound = 122;
+		}
+		final StringBuilder str = new StringBuilder();
+		for (int i = 0; i < length; i++) {
+			str.append((char) randInt(lowerBound, upperBound));
+		}
+		return str.toString();
+	}
+
+	private static int randInt(final int min, final int max) {
+		return ThreadLocalRandom.current().nextInt(min, max + 1);
 	}
 }
