24a00072637f0e6754b36d4bab8ee1a45c2a28f2
LieutenantPeacock <lieutenant.peacock@gmail.com>
2022-08-01 15:02:27 -0400

    Util: currently with method to check if user is authenticated.

---
+++ src/main/java/com/ltpeacock/dbviewer/util/Util.java
@@ -0,0 +1,17 @@
+package com.ltpeacock.dbviewer.util;
+
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class Util {
+	private Util() {
+		throw new UnsupportedOperationException();
+	}
+
+	public static boolean isAuthenticated() {
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		return authentication != null && !(authentication instanceof AnonymousAuthenticationToken)
+				&& authentication.isAuthenticated();
+	}
+}
