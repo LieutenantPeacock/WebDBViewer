7386e548b348ad35a92536ba41186e2c98eeb09c
LieutenantPeacock <lieutenant.peacock@gmail.com>
2022-08-01 16:03:18 -0400

    LoginController: add redirecting to setup or home when necessary.

--- src/main/java/com/colonelparrot/dbviewer/controller/LoginController.java
+++ src/main/java/com/colonelparrot/dbviewer/controller/LoginController.java
@@ -2,8 +2,12 @@ package com.colonelparrot.dbviewer.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.ltpeacock.dbviewer.commons.SetupInfo;
+import com.ltpeacock.dbviewer.util.Util;
 
 /**
  * @author ColonelParrot
@@ -12,9 +16,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 	private static final Logger LOG = LoggerFactory.getLogger(LoginController.class);
+	@Autowired
+	private SetupInfo setupInfo;
 
 	@GetMapping("/login")
-	public String redirectToViewer() {
-		return "login";
+	public String login() {
+		if (Util.isAuthenticated())
+			return "redirect:/";
+		return setupInfo.getAdminPassword() == null ? "login" : "redirect:/setup";
 	}
 }
