940ca77eea73eb063aca9bcd8d1474c2a0c4c487
LieutenantPeacock <lieutenant.peacock@gmail.com>
2022-08-03 00:21:49 -0400

    ConnectionForm.

---
+++ src/main/java/com/ltpeacock/dbviewer/form/ConnectionForm.java
@@ -0,0 +1,74 @@
+/**
+ *  Copyright (C) 2022  LieutenantPeacock
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+package com.ltpeacock.dbviewer.form;
+
+import javax.validation.constraints.NotBlank;
+
+/**
+ * @author LieutenantPeacock
+ */
+public class ConnectionForm {
+	@NotBlank(message = "JDBC URL must not be blank.")
+	private String url;
+	private String username;
+	private String password;
+	@NotBlank(message = "Driver path must not be blank.")
+	private String driverPath;
+	@NotBlank(message = "Driver name must not be blank.")
+	private String driverName;
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getDriverPath() {
+		return driverPath;
+	}
+
+	public void setDriverPath(String driverPath) {
+		this.driverPath = driverPath;
+	}
+
+	public String getDriverName() {
+		return driverName;
+	}
+
+	public void setDriverName(String driverName) {
+		this.driverName = driverName;
+	}
+}
