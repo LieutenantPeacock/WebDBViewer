fe8cf733551a97b173783c66229e1d3b9acb5bd6
LieutenantPeacock <lieutenant.peacock@gmail.com>
2022-08-03 00:32:38 -0400

    viewer.jsp + viewer.js: add ability to create new connection (mostly done).

--- src/main/webapp/WEB-INF/views/viewer.jsp
+++ src/main/webapp/WEB-INF/views/viewer.jsp
@@ -1,71 +1,166 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
-<%@ taglib prefix="sec" uri="http://www.springframework.org/security/tags"%>
+<%@ taglib prefix="sec"
+	uri="http://www.springframework.org/security/tags"%>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="_csrf" content="${_csrf.token}"/>
-<meta name="_csrf_header" content="${_csrf.headerName}"/>
+<meta name="_csrf" content="${_csrf.token}" />
+<meta name="_csrf_header" content="${_csrf.headerName}" />
 <title>Database Viewer - Grant</title>
-<link rel="stylesheet" href="<c:url value="/resources/css/viewer.css"/>" />
-<link rel="stylesheet" href="<c:url value="/resources/css/icons/icons.css"/>" />
+<link rel="stylesheet"
+	href="<c:url value="/resources/css/style.css"/>" />
+<link rel="stylesheet"
+	href="<c:url value="/resources/css/icons/icons.css"/>" />
+<link rel="stylesheet"
+	href="<c:url value="/resources/css/bootstrap-5.2.0.min.css"/>" />
+<style>
+@import
+	url('https://fonts.googleapis.com/css?family=Bebas+Neue&display=swap');
+
+#header {
+	background-color: #485761;
+	color: #fff;
+	font-family: 'Bebas Neue';
+	position: relative;
+	padding: 5px 10%;
+}
+
+#header h1 {
+	margin-bottom: 0;
+}
+
+#header p {
+	font-size: 25px;
+	margin-top: 0;
+	font-style: italic;
+}
+
+#logoutControl {
+	position: absolute;
+	top: 0;
+	right: 0;
+	font-size: 1.2em;
+}
+
+#logoutControl .material-icons {
+	padding: 10px;
+	background-color: white;
+	color: black;
+}
+
+#connections {
+	width: 30%;
+	border: 1px solid black;
+	flex-shrink: 0;
+	flex-grow: 0;
+	flex-basis: 30%;
+	max-width: 300px;
+}
+
+html, body {
+	height: 100%;
+}
+</style>
 </head>
-<body>
-	<div class="error-message">
-		<div class="error-icon">
-			<span class="material-icons">priority_high</span>
-		</div>
-		<div class="error-message-text">An error occured</div>
-		<div class="close-icon">
-			<span class="material-icons">close</span>
+<body class="d-flex flex-column">
+	<input type="hidden" id="basePath" value="<c:url value="/"/>"/>
+	<div id="header">
+		<h1>DB Viewer</h1>
+		<p>Lieutenant Peacock</p>
+		<sec:authorize access="isAuthenticated()">
+			<form method="POST" action="<c:url value="/logout"/>" title="Log Out">
+				<div id="logoutControl">
+					<button style="background: transparent; border: none; padding: 0;">
+						<span class="material-icons"> logout </span>
+					</button>
+				</div>
+				<sec:csrfInput />
+			</form>
+		</sec:authorize>
+	</div>
+	<div class="container-fluid flex-fill" style="overflow-y: auto;">
+		<div class="row h-100">
+			<div class="col" id="connections">
+				<h2 class="text-primary">Connections</h2>
+				<c:if test="${empty connections}">
+					<span style="font-weight: bold;">Currently no connections!</span>
+				</c:if>
+				<sec:authorize access="hasRole('ADMIN')">
+					<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#newConnectionModal">
+						Add New Connection
+					</button>
+				</sec:authorize>
+			</div>
+			<div class="col"></div>
 		</div>
 	</div>
-	<div class="container">
-		<div class="top" id="view-top">
-			<div class="header">
-				<h1>Database Viewer</h1>
-				<p>By: Grant</p>
-			</div>
-			<sec:authorize access="isAuthenticated()">
-				<form method="POST" action="<c:url value="/logout"/>">
-					<div class="controls">
-						<button style="background: transparent; border: none; padding: 0;">
-							<span class="material-icons" id="logout"> logout </span>
-						</button>
-					</div>
-					<sec:csrfInput/>
-				</form>
-			</sec:authorize>
-		</div>
-		<div class="content">
-			<div class="steps">
-				<div class="step">
-					<div class="custom-select-container" id="database-select">
-						<div class="custom-select-option preview">Select a database</div>
-						<div class="custom-select-options">
-							<c:forEach items="${databases}" var="item" varStatus="loop">
-								<div class="custom-select-option" data-value="${loop.index}">${item}</div>
-							</c:forEach>
-						</div>
-					</div>
+	<sec:authorize access="hasRole('ADMIN')">
+	<div class="modal fade" id="newConnectionModal" tabindex="-1"
+		aria-labelledby="exampleModalLabel" aria-hidden="true">
+		<div class="modal-dialog modal-dialog-scrollable">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="exampleModalLabel">New Connection</h5>
+					<button type="button" class="btn-close" data-bs-dismiss="modal"
+						aria-label="Close"></button>
 				</div>
-				<div class="step hidden" style="margin-left: 20px">
-					<div class="custom-select-container" id="table-select"></div>
+				<div class="modal-body">
+					<form class="needs-validation" novalidate id="newConnectionForm">
+					  <div class="mb-3">
+					    <label for="connection_url" class="form-label">JDBC URL</label>
+					    <input type="text" class="form-control" id="connection_url" name="url" required/>
+					    <div class="invalid-feedback">Please enter the JDBC URL.</div>
+					  </div>
+					  <div class="mb-3">
+					    <label for="connection_username" class="form-label">Username</label>
+					    <input type="text" class="form-control" id="connection_username" name="username"/>
+					  </div>
+					  <div class="mb-3">
+					    <label for="connection_password" class="form-label">Password</label>
+					    <input type="password" class="form-control" id="connection_password" name="password"/>
+					  </div>
+					  <hr/>
+					  <div>
+					  	<label for="connection_driverPath" class="form-label">Select JDBC Driver Location</label>
+					  	<select class="form-select" id="connection_driverPath" name="driverPath" required>
+					  		<c:forEach var="driver" items="${requestScope.drivers}">
+					  			<option value="${driver}">${driver}</option>
+					  		</c:forEach>
+					  	</select>
+					  	<div class="invalid-feedback" id="driverPathError">Please select the JDBC Driver Location.</div>
+					  	<div id="driverUploadContainer">
+					  	<label for="driverUpload" class="form-label">Or Upload New Driver (one or more JAR files)</label>
+					  	<input class="form-control no-auto-validate" type="file" id="driverUpload" multiple accept=".jar"/>
+					  	<div class="invalid-feedback" id="driverUploadError"></div>
+					  	<label for="driverFolderName" class="form-label">Driver Folder Name (required if more than one JAR file)</label>
+					  	<input class="form-control no-auto-validate" type="text" id="driverFolderName"/>
+					  	<div class="invalid-feedback" id="driverFolderError"></div>
+					  	<button type="button" class="btn btn-info mt-2 w-100" id="driverUploadBtn">Upload</button>
+					  	</div>
+					  </div>
+					  <div id="driverNameContainer" style="display: none;">
+					    <hr class="mt-3"/>
+					  	<label for="connection_driverName" class="form-label">Driver Name</label>
+					  	<select class="form-select" id="connection_driverName" name="driverName" required>
+					  	</select>
+					  </div>
+					  <div class="mt-3 text-danger" id="connectionFormError"></div>
+					 </form>
 				</div>
-			</div>
-			<div class="table-view">
-				<table>
-					<tbody>
-						<tr class="table-headers">
-						</tr>
-					</tbody>
-				</table>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary"
+						data-bs-dismiss="modal">Close</button>
+					<button type="submit" class="btn btn-outline-success" form="newConnectionForm">Save</button>
+				</div>
 			</div>
 		</div>
 	</div>
+	</sec:authorize>
+	<script src="<c:url value="/resources/js/bootstrap-5.2.0.bundle.min.js"/>"></script>
 	<script src="<c:url value="/resources/js/viewer.js"/>"></script>
 </body>
 </html>
--- src/main/webapp/resources/js/viewer.js
+++ src/main/webapp/resources/js/viewer.js
@@ -1,102 +1,100 @@
 ;(function(){
-	const errormessage = document.querySelector('.error-message');
-	errormessage.querySelector('.close-icon').addEventListener('click', function() {
-		errormessage.style.top = "-40px";
-	})
-	function showWarning(message) {
-		errormessage.querySelector('.error-message-text').textContent = message;
-		errormessage.style.top = "20px";
-	}
-	const databaseSelect = document.getElementById("database-select");
-	const tableSelect = document.getElementById("table-select");
-	const databaseOptions = databaseSelect
-			.querySelectorAll('.custom-select-option');
-	for (let i = 0; i < databaseOptions.length; i++) {
-		const current = databaseOptions[i];
-		current.addEventListener('click', databaseOptionClickHandler);
-	}
 	const csrfToken = document.querySelector("meta[name='_csrf']").content;
 	const csrfHeader = document.querySelector("meta[name='_csrf_header']").content;
-	function setCSRFToken(xhttp) {
-		xhttp.setRequestHeader(csrfHeader, csrfToken);
-	}
-	function databaseOptionClickHandler() {
-		const value = this.getAttribute('data-value');
-		var preview = databaseSelect.querySelector('.custom-select-option.preview');
-		preview.setAttribute('data-value', value);
-		preview.innerHTML = this.innerHTML;
-		var xhttp = new XMLHttpRequest();
-		xhttp.onreadystatechange = function() {
-			if (this.readyState == 4 && this.status == 200) {
-				databaseDataHandler(this.response);
-			}
-		};
-		xhttp.open("POST", "/?type=table&database=" + value, true);
-		setCSRFToken(xhttp);
-		xhttp.send();
-	}
-	function databaseDataHandler(response) {
-		const parsedJSON = JSON.parse(response);
-		if (parsedJSON.success == true) {
-			const tableNames = parsedJSON.data.tableNames;
-			tableSelect.innerHTML = "<div class='custom-select-option preview'>Select a table</div>";
-			tableSelect.parentElement.classList.remove('hidden');
-			const optionContainers = document.createElement("div");
-			optionContainers.classList.add('custom-select-options');
-			for ( const index in tableNames) {
-				const tableName = tableNames[index];
-				const optionElement = document.createElement("div");
-				optionElement.classList.add('custom-select-option');
-				optionElement.textContent = tableName;
-				optionElement.addEventListener('click', function() {
-					tableSelect.querySelector('.custom-select-option.preview').textContent = tableName;
-					const value = index;
-					var xhttp = new XMLHttpRequest();
-					xhttp.onreadystatechange = function() {
-						if (this.readyState == 4 && this.status == 200) {
-							tableDataHandler(this.response);
-						}
-					};
-					xhttp.open("POST", "/?type=row&table="
-							+ value
-							+ "&database="
-							+ databaseSelect.querySelector('.preview')
-									.getAttribute('data-value'), true);
-					setCSRFToken(xhttp);
-					xhttp.send();
+	const basePath = document.getElementById('basePath').value;
+	function _fetch(resource, options) {
+		return fetch(resource, {...options, headers: {...options?.headers, [csrfHeader]: csrfToken}})
+				.then(res => {
+					if(!res.ok) throw Error('Response status: ' + res.status);
+					return res.json();
 				});
-				optionContainers.appendChild(optionElement);
-			}
-			tableSelect.appendChild(optionContainers);
-		} else {
-			showWarning(parsedJSON.message);
-		}
 	}
-	function tableDataHandler(response) {
-		const parsedJSON = JSON.parse(response);
-		if (parsedJSON.success === true) {
-			const tableBody = document.querySelector('.table-view table tbody');
-			tableBody.innerHTML = "<tr class='table-headers'></tr>";
-			const data = parsedJSON.data;
-			const tableHeaders = document.querySelector('.table-headers');
-			for ( const columnName in data.columns) {
-				var header = document.createElement("th");
-				header.textContent = data.columns[columnName];
-				tableHeaders.appendChild(header);
+	const connectionFormError = document.getElementById('connectionFormError');
+	const newConnectionModal = document.getElementById('newConnectionModal');
+	let processing = false;
+	document.getElementById('newConnectionForm').addEventListener('submit', function(e){
+		e.preventDefault();
+		if (!processing) {
+			processing = true;
+			this.classList.add('was-validated');
+			connectionFormError.textContent = '';
+			if (this.checkValidity()) {
+				_fetch(basePath + 'newConnection', {method: 'POST', body: new FormData(this)})
+					.then(data => {
+						bootstrap.Modal.getInstance(newConnectionModal).hide();
+						console.log(data);
+					}).catch(error => {
+						connectionFormError.textContent = 'An error occurred.';
+					}).finally(()=>processing = false);
 			}
-			for ( const row in data.rows) {
-				const realRow = data.rows[row];
-				var rowElement = document.createElement("tr");
-				for ( const column in data.columns) {
-					var cell = document.createElement("td");
-					const columnName = data.columns[column];
-					cell.textContent = realRow[columnName];
-					rowElement.appendChild(cell);
+		}
+	});
+	const driverUploadContainer = document.getElementById('driverUploadContainer');
+	const driverUpload = document.getElementById('driverUpload');
+	const driverUploadError = document.getElementById('driverUploadError');
+	const driverFolderName = document.getElementById('driverFolderName');
+	const driverFolderError = document.getElementById('driverFolderError');
+	const driverPathSelect = document.getElementById('connection_driverPath');
+	const driverNameContainer = document.getElementById('driverNameContainer');
+	const driverPathError = document.getElementById('driverPathError');
+	const driverNameSelect = document.getElementById('connection_driverName');
+	driverPathSelect.selectedIndex = -1;
+	driverUpload.addEventListener('change', function(e){
+		this.classList.remove('is-invalid');
+	});
+	driverFolderName.addEventListener('input', function(e){
+		this.classList.remove('is-invalid');
+	})
+	document.getElementById('driverUploadBtn').addEventListener('click', function(e){
+		driverUploadContainer.querySelectorAll('.is-invalid')
+				.forEach(el => el.classList.remove('is-invalid'));
+		if (driverUpload.files.length) {
+			this.disabled = true;
+			const data = new FormData;
+			for(const file of driverUpload.files) 
+				data.append('files', file);
+			data.append('folder', driverFolderName.value);
+			_fetch(basePath + 'uploadDriver', {
+				method: 'POST',
+				body: data
+			}).then(data => {
+				for (const [inputId, error] of Object.entries(data.errors)) {
+					const input = driverUploadContainer.querySelector('#' + inputId);
+					input.nextElementSibling.textContent = error;
+					input.classList.add('is-invalid');
 				}
-				tableBody.appendChild(rowElement);
-			}
-		} else {
-			showWarning(parsedJSON.message);
+				if (data.success) {
+					driverPathSelect.add(new Option(data.value));
+					driverPathSelect.value = data.value;
+					onDriverPathChange();
+					driverUploadContainer.querySelectorAll('input')
+						.forEach(input => input.value = "");
+				}
+			}).catch(error => {
+				driverUploadError.textContent = 'An error occurred.';
+				driverUpload.classList.add('is-invalid');
+			}).finally(() => {
+				this.disabled = false;
+			});
 		}
+	});
+	function onDriverPathChange() {
+		driverPathSelect.classList.remove('is-invalid');
+		driverNameContainer.style.display = 'none';
+		_fetch(basePath + 'getDrivers?' + new URLSearchParams({driverPath: driverPathSelect.value}))
+		  .then(data => {
+			for(const driver of data) {
+				const option = document.createElement('option');
+				option.text = driver.name + (driver.deprecated ? ' (DEPRECATED)': '');
+				option.value = driver.name;
+				if (driver.deprecated) option.style.backgroundColor = '#808080';
+				driverNameSelect.add(option);
+			}
+			driverNameContainer.style.display = '';
+		}).catch(error => {
+			driverPathError.textContent = 'An error occurred fetching the drivers at the specified location.';
+			driverPathSelect.classList.add('is-invalid');
+		});
 	}
+	driverPathSelect.addEventListener('change', onDriverPathChange);
 })();
