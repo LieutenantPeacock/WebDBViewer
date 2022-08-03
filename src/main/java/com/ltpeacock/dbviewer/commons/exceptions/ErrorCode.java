9f3ffa4f14d8411d4b18308c4e76e674356aff55
LieutenantPeacock <lieutenant.peacock@gmail.com>
2022-08-03 09:56:18 -0400

    ErrorCode enum.

---
+++ src/main/java/com/ltpeacock/dbviewer/commons/exceptions/ErrorCode.java
@@ -0,0 +1,42 @@
+/**
+ *  Copyright (C) 2022  LieutenantPeacock
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+package com.ltpeacock.dbviewer.commons.exceptions;
+
+/**
+ * @author LieutenantPeacock
+ */
+public enum ErrorCode {
+	DRIVER_INITIALIZATION_ERROR("001", "Could not initialize Driver from JAR."),
+	CONNECTION_FAILED("002", "Could not use specified Driver to connect to JDBC URL");
+	
+	private final String code;
+	private final String message;
+
+	ErrorCode(final String code, final String message) {
+		this.code = code;
+		this.message = message;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+}
