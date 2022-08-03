05e969be8760100051fa9b1bdfee25f74b5d24e5
LieutenantPeacock <lieutenant.peacock@gmail.com>
2022-08-03 00:11:10 -0400

    DBConnectionDefDTO: to allow Jackson serialization.

---
+++ src/main/java/com/ltpeacock/dbviewer/db/dto/DBConnectionDefDTO.java
@@ -0,0 +1,65 @@
+/**
+ *  Copyright (C) 2022  LieutenantPeacock
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+package com.ltpeacock.dbviewer.db.dto;
+
+import com.ltpeacock.dbviewer.db.entity.DBConnectionDef;
+
+/**
+ * @author LieutenantPeacock
+ */
+public class DBConnectionDefDTO {
+	private final long id;
+	private final String url;
+	private final String driverPath;
+	private final String driverName;
+	private final String username;
+	private final String password;
+
+	public DBConnectionDefDTO(final DBConnectionDef def) {
+		this.id = def.getId();
+		this.url = def.getUrl();
+		this.driverPath = def.getDriverPath();
+		this.driverName = def.getDriverName();
+		this.username = def.getUsername();
+		this.password = def.getPassword();
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public String getDriverPath() {
+		return driverPath;
+	}
+
+	public String getDriverName() {
+		return driverName;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+}
