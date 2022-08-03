435999a574760b55ed34539dca647fee503db451
LieutenantPeacock <lieutenant.peacock@gmail.com>
2022-08-03 00:04:00 -0400

    Util: add method to convert BindingResult to Map of errors.

--- src/main/java/com/ltpeacock/dbviewer/util/Util.java
+++ src/main/java/com/ltpeacock/dbviewer/util/Util.java
@@ -1,8 +1,13 @@
 package com.ltpeacock.dbviewer.util;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 
 public class Util {
 	private Util() {
@@ -13,5 +18,10 @@ public class Util {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		return authentication != null && !(authentication instanceof AnonymousAuthenticationToken)
 				&& authentication.isAuthenticated();
+	}
+
+	public static Map<String, String> toErrorMap(final BindingResult result) {
+		return result.getFieldErrors().stream()
+				.collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage, (a, b) -> a + "\n" + b));
 	}
 }
