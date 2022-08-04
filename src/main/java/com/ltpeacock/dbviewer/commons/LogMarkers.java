c7897e612b52619f557df9438fe3240b1d25e92e
LieutenantPeacock <lieutenant.peacock@gmail.com>
2022-08-03 23:28:30 -0400

    LogMarkers: initialize with AUDIT Marker.

---
+++ src/main/java/com/ltpeacock/dbviewer/commons/LogMarkers.java
@@ -0,0 +1,32 @@
+/**
+ *  Copyright (C) 2022  LieutenantPeacock
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+package com.ltpeacock.dbviewer.commons;
+
+import org.slf4j.Marker;
+import org.slf4j.MarkerFactory;
+
+/**
+ * @author LieutenantPeacock
+ */
+public final class LogMarkers {
+	public static final Marker AUDIT = MarkerFactory.getMarker("AUDIT");
+
+	private LogMarkers() {
+		throw new UnsupportedOperationException();
+	}
+}
