96761cfba9fead2960b34b70535abcbb25ca327e
LieutenantPeacock <lieutenant.peacock@gmail.com>
2022-08-03 23:21:10 -0400

    Util: add replaceLineBreaks method.

--- src/main/java/com/ltpeacock/dbviewer/util/Util.java
+++ src/main/java/com/ltpeacock/dbviewer/util/Util.java
@@ -1,6 +1,7 @@
 package com.ltpeacock.dbviewer.util;
 
 import java.util.Map;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
@@ -14,6 +15,9 @@ public class Util {
 		throw new UnsupportedOperationException();
 	}
 
+	private static final Pattern CARRIAGE_RETURN_PAT = Pattern.compile("\r");
+	private static final Pattern NEWLINE_PAT = Pattern.compile("\n");
+
 	public static boolean isAuthenticated() {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		return authentication != null && !(authentication instanceof AnonymousAuthenticationToken)
@@ -23,5 +27,13 @@ public class Util {
 	public static Map<String, String> toErrorMap(final BindingResult result) {
 		return result.getFieldErrors().stream()
 				.collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage, (a, b) -> a + "\n" + b));
+	}
+
+	public static String replaceLineBreaks(final String str) {
+		return str == null ?
+				"null"
+				: NEWLINE_PAT.matcher(
+					CARRIAGE_RETURN_PAT.matcher(str).replaceAll("\\R")
+				).replaceAll("\\N");
 	}
 }
