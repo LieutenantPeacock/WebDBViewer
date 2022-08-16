380af1e7c6fb8f6c1da16349df365e22e2fb25fd
LieutenantPeacock <lieutenant.peacock@gmail.com>
2022-08-16 12:16:10 -0400

    Run setup on ApplicationReadyEvent instead of PostConstruct in main application class.

--- src/main/java/com/colonelparrot/dbviewer/GrantsDbViewerApplication.java
+++ src/main/java/com/colonelparrot/dbviewer/GrantsDbViewerApplication.java
@@ -6,15 +6,15 @@ import static com.colonelparrot.dbviewer.GrantsDbViewerApplication.LT_PEACOCK_DB
 import java.io.File;
 import java.util.concurrent.ThreadLocalRandom;
 
-import javax.annotation.PostConstruct;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.event.EventListener;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.ltpeacock.dbviewer.commons.AppProperties;
@@ -44,8 +44,8 @@ public class GrantsDbViewerApplication {
 		SpringApplication.run(GrantsDbViewerApplication.class, args);
 	}
 
-	@PostConstruct
-	public void setup() {
+	@EventListener(ApplicationReadyEvent.class)
+	private void setup() {
 		final File dbViewerBase = new File(appProps.getAppRoot());
 		LOG.info("DB Viewer base folder located at [{}]", dbViewerBase.getAbsolutePath());
 		if (!dbViewerBase.exists()) {
