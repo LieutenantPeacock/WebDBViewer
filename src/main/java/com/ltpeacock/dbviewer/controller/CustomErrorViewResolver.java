3ac0c38c7780569dfe4c24dd45f06ff44d792436
LieutenantPeacock <lieutenant.peacock@gmail.com>
2022-08-18 21:28:22 -0400

    Show raw error messages in error.jsp where applicable.

--- src/main/java/com/ltpeacock/dbviewer/controller/CustomErrorViewResolver.java
+++ src/main/java/com/ltpeacock/dbviewer/controller/CustomErrorViewResolver.java
@@ -17,16 +17,23 @@
  */
 package com.ltpeacock.dbviewer.controller;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorViewResolver;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.ltpeacock.dbviewer.commons.AppProperties;
+import com.ltpeacock.dbviewer.commons.LogMarkers;
+import com.ltpeacock.dbviewer.commons.RoleNames;
 
 /**
  * @author LieutenantPeacock
@@ -34,15 +41,26 @@ import org.springframework.web.servlet.ModelAndView;
 @Component
 public class CustomErrorViewResolver implements ErrorViewResolver {
 	private static final Logger LOG = LoggerFactory.getLogger(CustomErrorViewResolver.class);
+	@Autowired
+	private AppProperties appProps;
+
 	@Override
-	public ModelAndView resolveErrorView(final HttpServletRequest request, final HttpStatus status, 
+	public ModelAndView resolveErrorView(final HttpServletRequest request, final HttpStatus status,
 			final Map<String, Object> model) {
 		LOG.info("Error status [{}]", status);
+		final Map<String, Object> newModel = new HashMap<>(model);
+		final String view;
 		if (status == HttpStatus.NOT_FOUND) {
-			return new ModelAndView("404");
+			view = "404";
+		} else if (status == HttpStatus.FORBIDDEN) {
+			LOG.warn(LogMarkers.FORBIDDEN_ACCESS, "Forbidden access to [{}] from [{}]",
+					ServletUriComponentsBuilder.fromRequest(request).build().toString(), 
+					request.getRemoteAddr());
+			view = "access_denied";
+		} else {
+			newModel.put("showRawError", request.isUserInRole(RoleNames.ADMIN) || appProps.isShowRawExceptionsToUsers());
+			view = "error";
 		}
-		if (status == HttpStatus.FORBIDDEN)
-			return new ModelAndView("access_denied");
-		return new ModelAndView("error");
+		return new ModelAndView(view, newModel);
 	}
 }
--- src/main/webapp/WEB-INF/views/error.jsp
+++ src/main/webapp/WEB-INF/views/error.jsp
@@ -9,9 +9,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Web DBViewer | Error</title>
         <link rel="stylesheet" href="<c:url value="/resources/css/bootstrap-5.2.0.min.css"/>" />
+        <style>html, body {height: 100%;}</style>
     </head>
     <body>
-        <div class="d-flex align-items-center justify-content-center vh-100">
+        <div class="d-flex flex-column ${showRawError ? '' : 'justify-content-center align-items-center h-100'}">
             <div class="text-center">
             	<h1 class="display-1 fw-bold text-danger">An error occurred!</h1>
                 <p class="lead">
@@ -19,6 +20,20 @@
                  </p>
                 <a href="<c:url value="/"/>" class="btn btn-primary">Go Home</a>
             </div>
+            <c:if test="${showRawError}">
+	            <div class="mt-2 mx-2">
+	            	<h2>Details</h2>
+		            <table class="table table-bordered">
+		            	<tr><td>Date</td><td>${timestamp}</td></tr>
+		            	<tr><td>Path</td><td>${path}</td></tr>
+		            	<tr><td>Error</td><td>${error}</td></tr>
+		            	<tr><td>Status</td><td>${status}</td></tr>
+		            	<tr><td>Message</td><td>${message}</td></tr>
+		            	<tr><td>Exception</td><td>${exception}</td></tr>
+		            	<tr><td>Trace</td><td><pre><c:out value="${trace}"/></pre></td></tr>
+		            </table>
+	            </div>
+            </c:if>
         </div>
     </body>
 </html>
